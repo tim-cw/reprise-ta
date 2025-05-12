db546a9d01dfb5c03ec051fa32956e97926fb5a8
Jean-François Leblanc <jfleblanc@jfleblanc.com>
2025-05-12 12:59:14 -0400

    Initial commit

---
+++ src/scripts/Main.js
@@ -0,0 +1,13 @@
+import ComponentFactory from './ComponentFactory';
+
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    // Une petite chose à faire ici.
+    // new ComponentFactory();
+  }
+}
+new Main();
